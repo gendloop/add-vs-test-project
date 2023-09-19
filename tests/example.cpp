3525a527e1edbc382082f35c3eab9a1162a4feb6
gendloop <gendloop@163.com>
2023-09-19 15:53:35 +0800

    chore: update

--- tests/example.cpp
+++ tests/example.cpp
@@ -14,8 +14,16 @@
 #include <vector>
 // include-end
 
+// code
 TEST(Example, Case1) {
   DEBUG << "Example: Case1" << std::endl;
   
   INFO << "group_name: Example; case_name: Case1" << std::endl;
 }
+
+TEST(Example, Case2) {
+  DEBUG << "Example: Case2" << std::endl;
+  
+  INFO << "group_name: Example; case_name: Case2" << std::endl;
+}
+// code-end
